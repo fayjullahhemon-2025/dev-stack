f5919b531a4d320ddcb8bc8cae2b3342ab62343c
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-09-13 14:43:05 +0600

    learn more  (outlined) added

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -12,9 +12,9 @@ export default function Banner() {
                 <p className='text-[#475569] pb-3' >
                     Explore frontend, backend, database, and tooling options, <br /> compare them side by side, and put together the stack that fits your <br /> next project.
                 </p>
-                <div>
+                <div className='flex items-center gap-2' >
                     <button className='bg-linear-to-r from-orange-500 via-rose-500 to-pink-500 text-white font-semibold px-6 py-3 rounded-2xl shadow-md hover:opacity-95 transition' >Explore Technologies</button>
-                    <button className=' text-[#374151] font-semibold px-6 py-3 rounded-2xl shadow-md hover:opacity-95 transition'>Learn More</button>
+                    <button className=' text-[#374151] font-semibold px-6 py-3 rounded-2xl shadow-md hover:opacity-95 transition outline-[#E5E7EB]'>Learn More</button>
                 </div>
             </div>
             <div className='flex justify-center items-center '>
